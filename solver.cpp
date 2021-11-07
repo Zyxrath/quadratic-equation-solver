5e684da6caed3a2df7873f00975b8ceda2a3e366
O <ajthegreat0421@gmail.com>
2021-11-07 13:43:50 +0800

    Added the program

---
+++ solver.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a, b, c = "?";
+
+    cout << "Quadratic Equation Solver\n" << endl;
+    cout << " \n" << endl;
+    cout << "a = " << a << ", b = " << b << ", c = " << c << "\n" << endl;
+    cout << " \n" << endl;
+
+    return 0;
+}
