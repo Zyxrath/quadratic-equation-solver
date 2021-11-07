e1ae04cbe25394998b2e903d3d548ce9f1637c84
O <ajthegreat0421@gmail.com>
2021-11-07 14:11:59 +0800

    Added the values

--- solver.cpp
+++ solver.cpp
@@ -3,12 +3,19 @@ using namespace std;
 
 int main()
 {
-    string a, b, c = "?";
+    double a = 0, b = 0 , c = 0;
 
     cout << "Quadratic Equation Solver\n" << endl;
-    cout << " \n" << endl;
-    cout << "a = " << a << ", b = " << b << ", c = " << c << "\n" << endl;
-    cout << " \n" << endl;
+    cout << " a = " << a << ", b = " << b << ", c = " << c << "\n"<< endl;
+    cout << "Enter a number for a" << endl;
+    cin >> a;
+    cout << " a = " << a << ", b = " << b << ", c = " << c << "\n"<< endl;
+    cout << "Enter a number for b" << endl;
+    cin >> b;
+    cout << " a = " << a << ", b = " << b << ", c = " << c << "\n"<< endl;
+    cout << "Enter a number for c" << endl;
+    cin >> c;
+    cout << " a = " << a << ", b = " << b << ", c = " << c << "\n"<< endl;
 
     return 0;
 }
